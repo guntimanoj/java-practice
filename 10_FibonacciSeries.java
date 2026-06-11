1c5dd91a12239ec2d6adb0468c19933926c56c99
guntimanoj <guntimanoj01@gmail.com>
2026-06-11 13:46:58 +0530

    10_FibonacciSeries.java

---
+++ 10_FibonacciSeries.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public Class FibonacciSeries
+{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in)
+
+System.out.println("Enter number of terms: ");
+int n = sc.nextInt();
+
+int first = 0;
+int second = 1;
+
+System.out.println("Fibonacci Series: ");
+
+for(int i = 1; i <= n; i++)
+{
+System.out.println(first + " ");
+
+int next = first + second;
+first = second;
+second = next;
+}
+
+sc.close();
+}
