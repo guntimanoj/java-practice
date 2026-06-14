dede16061fd41aec407423d02e100196575c2f4b
guntimanoj <guntimanoj01@gmail.com>
2026-06-14 13:53:21 +0530

    16_AdditionUsingMethod.java

---
+++ 16_AdditionUsingMethod.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class AdditionUsingMethod
+{
+static int add(int a, int b)
+{
+return a + b;
+}
+
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter first number: ");
+int num1 = sc.nextInt();
+
+System.out.println("Enter second number: ");
+int num2 = sc.nextInt();
+
+int result = add(num1,num2);
+
+System.out.println("Sum = " + result);
+
+sc.close();
+}
+}
